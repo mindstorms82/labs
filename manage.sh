c3882129a632ea52368fbd5766ad5c68410b328e
mindstorms82 <mindstorms82@gmail.com>
2017-10-21 17:01:31 +0200

    network enterface generic

--- manage.sh
+++ manage.sh
@@ -12,8 +12,8 @@ filename80+="$dt-$filenamepre80"
 csv443="$filename443.csv"
 csv80="$filename80.csv"
 
-tshark -i virbr0 -w $filename443.pcap -f "tcp port 443" -a duration:200 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv443 |
-tshark -i virbr0 -w $filename80.pcap -f "tcp port 80" -a duration:200 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv80 | python3 ask.py
+tshark -i $2 -w $filename443.pcap -f "tcp port 443" -a duration:200 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv443 |
+tshark -i $2 -w $filename80.pcap -f "tcp port 80" -a duration:200 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv80 | python3 ask.py
 
 
 #./delete.sh

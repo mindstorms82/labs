7c055da55a560dd803f0e9b7c3f38ff03d3b8b1d
mindstorms82 <mindstorms82@gmail.com>
2017-10-22 19:00:39 +0200

    add conf

--- msql.py
+++ msql.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
 
 import sys, getopt
-
+from conf import *
 import pymysql.cursors
 
 import csv
 
 def read_csv(file_name):
     with open(file_name, newline='') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter='', quotechar=',')
-    print(csv_reader)
+        csv_reader = csv.reader(csvfile, delimiter=' ', quotechar=',')
     return csv_reader
 
 def sql(file_name):
-    config = {
-        'user': '',
-        'password': '',
-        'host': '',
-        'database': '',
-    }
-    cnx = pymysql.connect(host='',
-                             user='',
-                             password='',
-                             db='wireshark',)
+    cnx = pymysql.connect(host       = config['host'],
+                             user    = config['user'],
+                             password= config['password'],
+                             db      = config['db'])
     cursor = cnx.cursor()
     csv_data = read_csv(file_name)
-    for row in csv_data.iterrows():
+    for row in csv_data:
         list = row[1].values
         cursor.execute("INSERT INTO table1(frame.number,frame.time,ip.src,ip.dst,tcp.port,frame.len) VALUES('%d','%s','%s','%s','%s','%s','%s')" % tuple(list))
     cursor.close()
@@ -43,10 +36,9 @@ def main(argv):
          print('mysql.py -i <inputfile> -o')
          sys.exit()
       elif opt in ("-i", "--ifile"):
-         inputfile = arg
-   sql(inputfile)
+         inputfile = argv
+   sql(argv)
 
 if __name__ == "__main__":
-    print("goroo")
     main(sys.argv[1:])
 

2e61b9d34fa28cb0a37e5e84b6d6b20e4cf7220d
mindstorms82 <mindstorms82@gmail.com>
2017-10-25 21:14:31 +0200

    activate sql

--- msql.py
+++ msql.py
@@ -10,21 +10,26 @@ def read_csv(file_name):
     with open(file_name, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=' ', quotechar=',')
         for row in csv_reader:
-            print(row)
+            elements = row[0].split(",")
+            print(elements)
 
+def find_name(file_name):
+    for table in ["ebay","facebook","youtube","skype"]:
+        if table in file_name:
+            return table
+    return False
 def sql(file_name):
     cnx = pymysql.connect(host       = config['host'],
                              user    = config['user'],
                              password= config['password'],
                              db      = config['db'])
     cursor = cnx.cursor()
+    table_name = find_name(file_name)
     with open(file_name, newline='') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=' ', quotechar=',')
         for row in csv_data:
-            #row.replace(" ", "")
-            #srow.replace("CEST","")
-            print(row)
-            #cursor.execute("INSERT INTO table1(frame.number,frame.time,ip.src,ip.dst,tcp.port,frame.len) VALUES('%d','%s','%s','%s','%s','%s','%s')" % tuple(list))
+            elements = row[0].split(",") #frame.number,frame.time_relative,ip.src,ip.dst,tcp.port,frame.len,tcp.flags
+            cursor.execute("INSERT INTO table_name(frame.number,frame.time_relative,ip.src,ip.dst,tcp.port,frame.len,tcp.flags) VALUES('%d','%f','%s','%s','%d','%d','%s')", elements)
     cursor.close()
     cnx.close()
 

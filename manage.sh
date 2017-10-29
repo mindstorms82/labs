824cb42b6ab61ea8f2ff7c7fec0ece09b67e8400
mindstorms82 <mindstorms82@gmail.com>
2017-10-29 22:23:42 +0100

    update project

--- manage.sh
+++ manage.sh
@@ -12,13 +12,17 @@ filename80+="$dt-$filenamepre80"
 csv443="$filename443.csv"
 csv80="$filename80.csv"
 
-tshark -i $2 -w $filename443.pcap -f "tcp port 443" -a duration:20 -T fields -e frame.number -e  frame.time_relative -e ip.src -e ip.dst -e tcp.port -e frame.len -e tcp.flags  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv443 |
-tshark -i $2 -w $filename80.pcap -f "tcp port 80" -a duration:20 -T fields -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv80 | python3 ask.py
+tshark -i $2 -w $filename443.pcap -f "tcp port 443" -a duration:400 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len -e tcp.flags  -E header=y -E separator=, -E occurrence=f> $csv443 |
+tshark -i $2 -w $filename80.pcap -f  "tcp port 80"  -a duration:400 -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e tcp.port -e frame.len -e tcp.flags  -E header=y -E separator=, -E occurrence=f> $csv80 | python3 ask.py
+#tshark -i $2 -w $filename80.pcap -f "tcp port 80" -a duration:20 -T fields -e frame.number -e frame.time_relative -e ip.src -e ip.dst -e tcp.port -e frame.len  -E header=y -E separator=, -E quote=d -E occurrence=f> $csv80 | python3 ask.py
 
 pkill firefox
 killall firefox
 
+sleep 30
 
+python3 msql.py $csv443
+python3 msql.py $csv80
 
 
 #./delete.sh
--- msql.py
+++ msql.py
@@ -3,7 +3,7 @@
 import sys, getopt
 from conf import *
 import pymysql.cursors
-
+import re
 import csv
 
 def read_csv(file_name):
@@ -16,6 +16,7 @@ def read_csv(file_name):
 def find_name(file_name):
     for table in ["ebay","facebook","youtube","skype"]:
         if table in file_name:
+            table = re.search('\w+', table).group(0)
             return table
     return False
 
@@ -25,33 +26,39 @@ def connect_db():
                              password= config['password'],
                              db      = config['db'])
     cursor = cnx.cursor()
-    return cursor
+    return cursor, cnx
 
-def create_table(file_name):
-    cursor = connect_db(file_name)
-
+def create_tables():
+    cursor, cnx = connect_db()
     #to create a table:
-    cursor.execute('CREATE TABLE IF NOT EXISTS `wireshark`.`%s` (`frame.number` int(4), `frame.time_relative` decimal(11,9), `ip.src` varchar(15), `ip.dst` varchar(15), `tcp.port` int(5), `frame.len` int(4), `tcp.flags` varchar(10)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci', file_name)
-
+    for table in ['ebay', 'facebook', 'youtube', 'skype']:
+        table = re.search('\w+', table).group(0)
+        cursor.execute('CREATE TABLE IF NOT EXISTS `wireshark`.`%s`(`frame.number` int(4), `day` int(4), `year` int(4), `frame.time` varchar(30), `ip.src` varchar(15), `ip.dst` varchar(15), `tcp.port` int(5), `frame.len` int(4), `tcp.flags` varchar(10)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci', (table))
+        cnx.commit()
 
 
 def sql(file_name):
     table_name = find_name(file_name)
-    cursor = connect_db()
-
-    print(cursor.execute("SELECT * FROM `%s`", table_name))
-    create_table(table_name)
-    print(cursor.execute("SELECT * FROM `%s`", table_name))
-    if not cursor.execute("SELECT * FROM `%s`", table_name):
-        pass
+    cursor, cnx = connect_db()
 
     with open(file_name, newline='') as csvfile:
         csv_data = csv.reader(csvfile, delimiter=' ', quotechar=',')
+        next(csv_data)
         for row in csv_data:
-            elements = row[0].split(",") #frame.number,frame.time_relative,ip.src,ip.dst,tcp.port,frame.len,tcp.flags
-            print()
-            cursor.execute("INSERT INTO `wireshark`.`%s` (`frame.number`, `frame.time_relative`, `ip.src`, `ip.dst`, `tcp.port`, `frame.len`, `tcp.flags`)  VALUES('%s','%s','%s','%s','%s','%s','%s')", table_name, elements)
-    cursor.close()
+            row[0] = row[0].replace(',Oct', '')
+            row[1] = row[1].replace(',','')
+            row[4] = row[4].replace('CET,', '')
+            elements = row[4].split(",")
+            i = 4
+            for element in elements:
+                row.insert(i, element)
+                i = i + 1
+            row = row[:-1] #remove last element
+            print(row)
+            row.insert(0, table_name)
+            cursor.execute("INSERT INTO `%s` (`frame.number`, `day`, `year`, `frame.time`, `ip.src`, `ip.dst`, `tcp.port`, `frame.len`, `tcp.flags`)  VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (row))
+            cnx.commit()
+    cnx.close()
 
 def main(argv):
    inputfile = ''
@@ -66,6 +73,7 @@ def main(argv):
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = argv
+   #create_tables()
    sql(argv)
 
 if __name__ == "__main__":

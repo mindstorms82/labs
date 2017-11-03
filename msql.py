556ddf9e022c5aacf9892dfd56ba3ce515c911e6
mindstorms82 <mindstorms82@gmail.com>
2017-11-03 15:43:23 +0100

    fix bug

--- msql.py
+++ msql.py
@@ -14,7 +14,7 @@ def read_csv(file_name):
             print(elements)
 
 def find_name(file_name):
-    for table in ["ebay","facebook","youtube","skype"]:
+    for table in ["ebay","facebook","youtube","skype", "mix"]:
         if table in file_name:
             table = re.search('\w+', table).group(0)
             return table

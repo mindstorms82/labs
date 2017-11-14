385d56b41e58f4b85df176326d78b26c9a267b7b
mindstorms82 <mindstorms82@gmail.com>
2017-11-14 22:23:35 +0100

    add comments

--- analyse.py
+++ analyse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys, time, getopt
+import sys, getopt
 import numpy as np
 from sklearn.metrics import jaccard_similarity_score
 from msql import connect_db
@@ -15,28 +15,28 @@ model_names = (
     'youtube'
  )
 
-def mysql_pred(cursor, cnx, table_name, lower_limit = 100, upper_limit= 120):
+def mysql_pred(cursor, cnx, table_name, lower_limit= 101, upper_limit= 116):
     result = []
-    cursor.execute("SELECT `frame.number`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
+    print(table_name)
+    cursor.execute("SELECT `id`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
     cnx.commit()
-    for len in cursor:
-        if int(lower_limit) <= len[0] <= int(upper_limit):
-            result.append((len[1]))
+    for lene in cursor:
+        if int(lower_limit) < lene[0] <= int(upper_limit):
+            result.append((lene[1]))
     return result
 ## END Function ##
 
 def mysql_true(cursor, cnx, table_name, lower_limit = 0, upper_limit = 0):
-    print(table_name)
     result = []
     cursor.execute("SELECT COUNT(*) FROM `%s`", (table_name))
     cnx.commit()
     number_of_rows = cursor.fetchone()[0]
-
-    cursor.execute("SELECT `frame.number`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
+    cursor.execute("SELECT `id`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
     cnx.commit()
-    for len in cursor:
-        if int(lower_limit) <= len[0] <= int(upper_limit):
-            result.append((len[1]))
+    for lene in cursor:
+        if int(lower_limit) < lene[0] <= int(upper_limit):
+            print("Row Nr.: ", lene[0])
+            result.append((lene[1]))
     return result
 ## END Function ##
 
@@ -52,7 +52,7 @@ def average_value(cursor, cnx, table_name):
 
 def maximum_value(cursor, cnx, table_name):
     result=[]
-    cursor.execute("SELECT `frame.number`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
+    cursor.execute("SELECT `id`, `frame.len`, `tcp.port` FROM `%s`", (table_name))
     cnx.commit()
     for lene in cursor:
         result.append(lene[1])
@@ -81,49 +81,38 @@ def graph(cursor, cnx, values, lower_limit, upper_limit):
     plt.show()
 ## END Function ##
 
-def test():
-    ticks = 11
-    j = 0
-    i = 0
-    for i in range(1, 50, ticks):
-        j = j + 11
-        print(i, j)
-
-
 def compare(table_name_true, table_name_pred, graphix=False):
     cursor, cnx = connect_db()
     match_list = []
+    average_values = []
+    maximum_values = []
+    ticks = 15
+    upper_limit = 0
+    output = []
+    output_persentage = []
 
     if graphix:
-        average_values = []
-        maximum_values = []
         for table in model_names:
             average_values.append(round(average_value(cursor,cnx, table)))
             #maximum_values.append(maximum_value(cursor, cnx, table))
         graph(cursor, cnx, average_values, min(average_values), max(average_values))
 
     max = get_maximum_input(cursor, cnx, table_name_true)
-    ticks = 11
     packet_pred = mysql_pred(cursor, cnx, table_name_pred)
+
     for lower_limit in range(1, max, ticks):
-        #upper_limit = lower_limit + ticks
         upper_limit = max if upper_limit > max else lower_limit + ticks
         packet_true = mysql_true(cursor, cnx, table_name_true, lower_limit, upper_limit)
+        print(packet_pred)
+        print(packet_true)
+        output.append(jaccard_similarity_score(packet_true , packet_pred, normalize=False))
+        output_persentage.append(jaccard_similarity_score(packet_true , packet_pred))
 
-        #if len(skype_pred) > len(skype_true):
-        #    skype_pred = skype_pred[0:len(skype_true)]
-        #elif len(skype_true) > len(skype_pred):
-        #    skype_true = skype_true[0:len(skype_pred)]
-
-        output = jaccard_similarity_score(packet_true , packet_pred, normalize=False)
-        output_persentage = jaccard_similarity_score(packet_true , packet_pred)
-        print("Library name: ", table_name_true, "\n")
-        print("Length Range: %s - %s", (lower_limit, upper_limit))
-        print("Matches in totoal: " , output)
-        print("Matches in Persentage: " , (100 * output_persentage))
+    print("Library name: ", table_name_true, "\n")
+    #print("Length Range: ", (lower_limit, upper_limit))
+    #print("Matches in totoal: " , output)
+    print("Similarity in Persentage: " , (100 * max(output_persentage)))
 ## END Function ##
-
-
 
 
 ## Main Function ##
@@ -133,14 +122,7 @@ if __name__ == "__main__":
     except getopt.GetoptError:
         print('analyse.py -i <inputfile>')
         sys.exit(2)
-
-    test()
-    exit(0)
     compare(sys.argv[1], sys.argv[2])
-    #compare("facebook", sys.argv[2])
-    #compare("youtube", sys.argv[2])
-    #compare("skype", sys.argv[2])
-
 
 
 

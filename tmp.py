b614ed8b6138248af95958663f0dfe9fa87d3950
yuris82 <yuris82@j1>
2017-12-01 18:59:44 +0100

    add tmp file

---
+++ tmp.py
@@ -0,0 +1,31 @@
+        #opts, args = getopt.getopt(sys.argv[1:], "hs:r:v", ["help", "single=", "range="])
+
+    #except getopt.GetoptError as err:
+    #    print(err)
+    #    usage()
+    #    sys.exit(2)
+    #output = None
+    #erbose = False
+    #for o, a in args:
+#        print(o, a)
+#        if o == "-v":
+#            verbose = True
+#        if o in ("-h", "--help"):
+#            usage()
+#            sys.exit()
+#        elif o in ("-s", "--single"):
+#            plot_statistic(value=a)
+#        elif o in ("-r", "--range"):
+#            plot_similar()
+#        else:
+#            assert False, "unhandled option"
+
+
+        # for value in values:
+        # value = 54
+        # value = 66
+        # value = 275
+        # value = 2037
+        # value = 192 # aus ebay
+        # value = 308
+        # value = 1506
